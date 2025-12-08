98aaf6ea77f1e4e5cdefa4d57c581b409ac06a64
Paul Bishop <paulbishop@iMac.lan>
2025-12-08 19:12:32 +0000

    v19

--- src/views/SpeechLanguageNeedsPage.vue
+++ src/views/SpeechLanguageNeedsPage.vue
@@ -364,7 +364,7 @@
           <ion-card-content>
             <div class="case-study-images">
               <img
-                :src="getPublicUrl('speech_case_study.jpg')"
+                :src="getPublicUrl('speech_case_study.png')"
                 alt="Learner participating in a speech and language activity"
                 class="case-study-image"
               />
